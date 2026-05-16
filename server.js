843d0c62a24db7e8d9f26950fe75b4451fb5dcd1
Wertoquri <kirilpojduk@gmail.com>
2026-05-16 17:50:37 +0300

    make canvas

--- server.js
+++ server.js
@@ -24,12 +24,60 @@ const server = createServer(async(req, res)=> {
 
 const io = new Server(server)
 
+let ball = {x: 300, y: 200, vx: 4, vy: 3}
+let isPlaying = false;
+let players = {}
+let sides = ["left", "right"]
+
 io.on("connection", (socket)=>{
     console.log(
         chalk.green("user connected with id:"),
         chalk.yellow(socket.id)
     )
+
+    if(Object.keys(players).length < 2){
+        const takenSides = Object.values(players).map(p => p.side)
+        const freeSide = sides.find(s => !takenSides.includes(s))
+
+
+        players[socket.id] = {
+            y: 150,
+            sides: freeSide
+        }
+    }
+
+    socket.emit("init", {
+        id: socket.id,
+        players
+    })
+
+    socket.on("start", ()=> {
+        isPlaying = true;
+
+        ball.x = 300
+        ball.y = 200
+        ball.vx = 4 * (Math.random() > 0.5 ? 1: -1)
+        ball.vy = 3 * (Math.random() > 0.5 ? 1: -1)
+    })
 })
+
+setInterval(()=>{
+    if(!isPlaying) {
+        io.emit("state", {ball, isPlaying})
+        return
+    }
+
+    ball.x += ball.vx
+    ball.y += ball.vy
+
+    if(ball.y <= 0 || ball.y >= 400) ball.vy *= -1
+    if(ball.x <= 0 || ball.x >= 600) ball.vx *= -1
+
+    ball.vx += ball.vx * 0.001
+    ball.vy += ball.vy * 0.001
+
+    io.emit("state", {ball, isPlaying})
+}, 1000/60)
 
 server.listen(3000, ()=> console.log("server on!"))
 
--- static/script.js
+++ static/script.js
@@ -1,1 +1,46 @@
 const socket = io()
+
+const canvas = document.getElementById("game")
+const ctx = canvas.getContext("2d")
+const startBtn = document.getElementById("startBtn")
+
+startBtn.addEventListener("click", () => socket.emit("start"))
+
+let ball = {}
+let myId = null
+let players = {}
+
+let myY = 100;
+canvas.addEventListener("mousemove", (e)=> {
+    const rect = canvas.getBoundingClientRect()
+    const y = e.clientY - rect.top
+    myY = y
+})
+
+socket.on("init", (data)=>{
+    myId = data.id
+    players = data.players
+})
+function draw(){
+    ctx.clearRect(0, 0, 600, 400)
+
+    for(let id in players){
+        let p = players[id]
+        
+    
+    ctx.fillRect(
+        p.side == "left" ? 10 : 580,
+        myY,
+        10,
+        100
+    )
+    }
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, 8, 0, 2 * Math.PI)
+    ctx.fill()
+}
+
+socket.on("state", state => {
+    ball = state.ball
+    draw()
+})
